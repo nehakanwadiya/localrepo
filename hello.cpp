60e08e165f31a1c5e4c5cce275bcd7f96190a63e
Neha Kanwadiya <kanwadiyaneha4@gmail.com>
2024-12-10 18:22:13 +0530

    add initial files

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    cout<<"Hello World, this is neha."<<endl;
+    cout<<"I am starting a journey on github."<<endl;
+}
---
+++ sum.py
@@ -0,0 +1,3 @@
+print(5+3)
+print(6+9)
+print("here is the sum given of four numbers")
